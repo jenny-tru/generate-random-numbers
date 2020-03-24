f84eaccb8d207cefef9ac0d7dc37085c75326fc5
jenny-tru <jennytru@DESKTOP-PHUSTMO.localdomain>
2020-03-24 00:39:41 -0400

    :tada: randomization works!!!

--- number.py
+++ number.py
@@ -4,12 +4,16 @@ import random
 people = raw_input("How many gifts are under the tree? ")
 print("Generating random numbers for a party of " + people + "... ")
 print()
+""" if people is not 'None'
+        randomlist.append(n) """
 
 randomlist = []
-for i in range(0, 6):
-    n = random.randint(1,10)
+for i in range(0, int(people)):
+    n = random.randint(1,20)
     if n not in randomlist: randomlist.append(n)
-    if people
+   
+        
+    
     
 print(randomlist)
 """ print(random.choice())
