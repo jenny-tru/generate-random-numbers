4f9970cfd38af8adfa87005fb1e58ecdd51a174e
jenny-tru <jennytru@DESKTOP-PHUSTMO.localdomain>
2020-03-27 14:17:37 -0400

    not printing full amount yet

--- number.py
+++ number.py
@@ -1,15 +1,14 @@
 from pip._vendor.distlib.compat import raw_input
 import random
 
-people = raw_input("How many gifts are under the tree? ")
-print("Generating random numbers for a party of " + people + "... ")
-print()
+amount = raw_input("How many gifts are under the tree? ")
+print("Generating random numbers for a party of " + amount + "... \n" )
 """ if people is not 'None'
         randomlist.append(n) """
 
 randomlist = []
-for i in range(0, int(people)):
-    n = random.randint(1,50)
+for i in range(0, int(amount)):
+    n = random.randint(1,20)
     if n not in randomlist: randomlist.append(n)
    #need to print full amount from input and not repeat values
 print(randomlist)
